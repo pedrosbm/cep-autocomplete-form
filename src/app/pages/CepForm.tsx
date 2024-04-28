b1356b04c38da3dad48345f4dcd7a0b233a4b2d7
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-04-28 01:18:21 -0300

    formulario estilizado

--- src/app/pages/CepForm.tsx
+++ src/app/pages/CepForm.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { useState } from "react";
 import { Endereco } from "../types/Endereco";
 
+import "../style/formulario.css"
+
 const CepForm = () => {
 
     const [endereco, setEndereco] = useState<Endereco>({})
@@ -35,60 +37,64 @@ const CepForm = () => {
     }
 
     return (
-        <>
-            <form>
+        <section>
+            <form onSubmit={e => {e.preventDefault()}}>
                 <div className="inputBox">
-                    <label htmlFor="cep">Cep:</label><br />
-                    <input onBlur={handleCep} onChange={handleChange} minLength={8} type="text" name="cep" id="cep" /><br />
-                </div>
-                <span style={{color: "#e00404"}} id="notFoundField">{notFound == true ? "Endereço não encontrado, verifique o cep" : ""}</span>
-
-                <div className="inputBox">
-                    <label htmlFor="logradouro">Logradouro:</label><br />
-                    <input value={endereco?.logradouro} onChange={handleChange} type="text" name="logradouro" id="logradouro" /><br />
+                    <label htmlFor="cep">Cep:</label>
+                    <input placeholder="bruh" onBlur={handleCep} onChange={handleChange} minLength={8} type="text" name="cep" id="cep" />
                 </div>
 
+                <span style={{ color: "#e00404" }} id="notFoundField">{notFound == true ? "Endereço não encontrado, verifique o cep" : ""}</span>
+
                 <div className="inputBox">
-                    <label htmlFor="complemento">complemento:</label><br />
-                    <input value={endereco?.complemento} onChange={handleChange} type="text" name="complemento" id="complemento" /><br />
+                    <label htmlFor="logradouro">Logradouro:</label>
+                    <input placeholder="bruh" value={endereco?.logradouro} onChange={handleChange} type="text" name="logradouro" id="logradouro" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="bairro">bairro:</label><br />
-                    <input value={endereco?.bairro} onChange={handleChange} type="text" name="bairro" id="bairro" /><br />
+                    <label htmlFor="complemento">complemento:</label>
+                    <input placeholder="bruh" value={endereco?.complemento} onChange={handleChange} type="text" name="complemento" id="complemento" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="localidade">localidade:</label><br />
-                    <input value={endereco?.localidade} onChange={handleChange} type="text" name="localidade" id="localidade" /><br />
+                    <label htmlFor="bairro">bairro:</label>
+                    <input placeholder="bruh" value={endereco?.bairro} onChange={handleChange} type="text" name="bairro" id="bairro" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="uf">uf:</label><br />
-                    <input value={endereco?.uf} type="text" onChange={handleChange} name="uf" id="uf" /><br />
+                    <label htmlFor="localidade">localidade:</label>
+                    <input placeholder="bruh" value={endereco?.localidade} onChange={handleChange} type="text" name="localidade" id="localidade" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="ibge">ibge:</label><br />
-                    <input value={endereco?.ibge} type="text" onChange={handleChange} name="ibge" id="ibge" /><br />
+                    <label htmlFor="uf">uf:</label>
+                    <input placeholder="bruh" value={endereco?.uf} type="text" onChange={handleChange} name="uf" id="uf" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="gia">gia:</label><br />
-                    <input value={endereco?.gia} type="text" onChange={handleChange} name="gia" id="gia" /><br />
+                    <label htmlFor="ibge">ibge:</label>
+                    <input placeholder="bruh" value={endereco?.ibge} type="text" onChange={handleChange} name="ibge" id="ibge" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="ddd">ddd:</label><br />
-                    <input value={endereco?.ddd} type="text" onChange={handleChange} name="ddd" id="ddd" /><br />
+                    <label htmlFor="gia">gia:</label>
+                    <input placeholder="bruh" value={endereco?.gia} type="text" onChange={handleChange} name="gia" id="gia" />
                 </div>
 
                 <div className="inputBox">
-                    <label htmlFor="siafi">siafi:</label><br />
-                    <input value={endereco?.siafi} type="text" onChange={handleChange} name="siafi" id="siafi" /><br />
+                    <label htmlFor="ddd">ddd:</label>
+                    <input placeholder="bruh" value={endereco?.ddd} type="text" onChange={handleChange} name="ddd" id="ddd" />
                 </div>
+
+                <div className="inputBox">
+                    <label htmlFor="siafi">siafi:</label>
+                    <input placeholder="bruh" value={endereco?.siafi} type="text" onChange={handleChange} name="siafi" id="siafi" />
+                </div>
+
+                <button className="submit">Enviar</button>
             </form>
-        </>
+
+        </section>
     )
 }
 
